283e0098de682a5fb2982e012c385c78307e0d82
GandziukVI <gandziuk2009@gmail.com>
2018-07-19 11:27:27 +0200

    Basic IExperiment interface implementation

---
+++ BWOSetup/iexperiment.h
@@ -0,0 +1,29 @@
+#ifndef IEXPERIMENT_H
+#define IEXPERIMENT_H
+
+#include <QFuture>
+#include <QMutex>
+#include <QObject>
+
+class IExperiment : public QObject
+{
+    Q_OBJECT
+public:
+    IExperiment();
+    IExperiment(QObject *expSettings);
+    ~IExperiment();
+
+    virtual void start();
+    virtual void stop();
+    virtual void toDo() = 0;
+    virtual void toDo(QObject *expSettings) = 0;
+
+    bool    mExperimentIsRunning;
+    QObject *mExpSettings;
+
+private:
+    mutable QMutex mIExperimentMutex;
+    QFuture<void>  mExpThreadRes;
+};
+
+#endif // IEXPERIMENT_H
