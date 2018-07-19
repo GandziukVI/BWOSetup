d1596b82350524bb7fff00ac8ba4c1b893580f56
GandziukVI <gandziuk2009@gmail.com>
2018-07-19 11:59:08 +0200

    Minimum working wxample with real device

--- BWOSetup/BWOExperiment.cpp
+++ BWOSetup/BWOExperiment.cpp
@@ -1,5 +1,10 @@
 #include "BWOExperiment.h"
+
+// For debugging
 #include <QDebug>
+#include <QThread>
+
+#define DAQmxErrChk(functionCall) if( DAQmxFailed(error=(functionCall)) ) throw error
 
 BWOExperiment::BWOExperiment()
 {
@@ -8,10 +13,100 @@ BWOExperiment::BWOExperiment()
 
 void BWOExperiment::toDo()
 {
-    qDebug() << "To do function started";
+    try
+    {
+        DAQmxErrChk (DAQmxCreateTask("",&hTaskInput));
+        DAQmxErrChk (DAQmxCreateAIVoltageChan(hTaskInput,"Dev1/ai3","",DAQmx_Val_Cfg_Default,MIN_VOLTAGE_VALUE,MAX_VOLTAGE_VALUE,DAQmx_Val_Volts,NULL));
+        DAQmxErrChk (DAQmxStartTask(hTaskInput));
+
+        DAQmxErrChk (DAQmxCreateTask("Output Voltage Task",&hTaskOutput));
+        DAQmxErrChk (DAQmxCreateAOVoltageChan(hTaskOutput,"Dev1/ao0","", MIN_VOLTAGE_VALUE,MAX_VOLTAGE_VALUE,DAQmx_Val_Volts,""));
+        DAQmxErrChk (DAQmxStartTask(hTaskOutput));
+        DAQmxErrChk (DAQmxWriteAnalogScalarF64(hTaskOutput,false,4,1.543,NULL));
+        QThread::msleep(5000);
+        for(int i = 1; i <= 30; i++)
+        {
+            DAQmxErrChk (DAQmxWriteAnalogScalarF64(hTaskOutput,false,4,i*0.1,NULL));
+            QThread::msleep(500);
+            qDebug() << "Current voltage value is " << i*0.1;
+            //            cout << i*0.001;
+            //            DAQmxErrChk (DAQmxReadAnalogScalarF64(hTaskInput,4,&voltageValue,NULL));
+            //            fout << i*0.1 << "\t" << voltageValue << endl;
+            //            Sleep(1000);
+        }
+    }
+    catch(int error)
+    {
+        DAQmxGetExtendedErrorInfo(errBuff,2048);
+        qDebug() << "DAQmx Error: " << errBuff;
+    }
+    catch (...)
+    {
+        qDebug() << "Unknown exception caught\n";
+    }
+
+    if( hTaskInput!=0 )  {
+        DAQmxStopTask(hTaskInput);
+        DAQmxClearTask(hTaskInput);
+    }
+    if( hTaskOutput!=0 )  {
+        DAQmxStopTask(hTaskOutput);
+        DAQmxClearTask(hTaskOutput);
+    }
+
+    qDebug() << "To do function ended";
 }
 
 void BWOExperiment::toDo(QObject *expSettings)
 {
-    qDebug() << "To do function started";
+    try
+    {
+        DAQmxErrChk (DAQmxCreateTask("",&hTaskInput));
+        DAQmxErrChk (DAQmxCreateAIVoltageChan(hTaskInput,"Dev1/ai3","",DAQmx_Val_Cfg_Default,MIN_VOLTAGE_VALUE,MAX_VOLTAGE_VALUE,DAQmx_Val_Volts,NULL));
+        DAQmxErrChk (DAQmxStartTask(hTaskInput));
+
+        DAQmxErrChk (DAQmxCreateTask("Output Voltage Task",&hTaskOutput));
+        DAQmxErrChk (DAQmxCreateAOVoltageChan(hTaskOutput,"Dev1/ao0","", MIN_VOLTAGE_VALUE,MAX_VOLTAGE_VALUE,DAQmx_Val_Volts,""));
+        DAQmxErrChk (DAQmxStartTask(hTaskOutput));
+        DAQmxErrChk (DAQmxWriteAnalogScalarF64(hTaskOutput,false,4,1.543,NULL));
+        QThread::msleep(5000);
+        for(int i = 1; i <= 30; i++)
+        {
+            DAQmxErrChk (DAQmxWriteAnalogScalarF64(hTaskOutput,false,4,i*0.1,NULL));
+            QThread::msleep(1000);
+            qDebug() << "Current voltage value is " << i*0.1;
+            //            cout << i*0.001;
+            //            DAQmxErrChk (DAQmxReadAnalogScalarF64(hTaskInput,4,&voltageValue,NULL));
+            //            fout << i*0.1 << "\t" << voltageValue << endl;
+            //            Sleep(1000);
+        }
+    }
+    catch(int error)
+    {
+        DAQmxGetExtendedErrorInfo(errBuff,2048);
+        qDebug() << "DAQmx Error: " << errBuff;
+    }
+    catch (...)
+    {
+        qDebug() << "Unknown exception caught\n";
+    }
+
+    if( hTaskInput!=0 )  {
+        DAQmxStopTask(hTaskInput);
+        DAQmxClearTask(hTaskInput);
+    }
+    if( hTaskOutput!=0 )  {
+        DAQmxStopTask(hTaskOutput);
+        DAQmxClearTask(hTaskOutput);
+    }
+
+    qDebug() << "To do function ended";
+}
+
+void BWOExperiment::stop()
+{
+    // To DO:
+    // Add voltage reset when experiment is finished
+
+    IExperiment::stop();
 }
--- BWOSetup/BWOExperiment.h
+++ BWOSetup/BWOExperiment.h
@@ -3,12 +3,33 @@
 
 #include "IExperiment.h"
 
+// Working with NI DAQ MX
+#include<NIDAQmx.h>
+
 class BWOExperiment : public IExperiment
 {
 public:
     BWOExperiment();
     void toDo() override;
     void toDo(QObject *expSettings) override;
+
+private:
+    const float64   MAX_VOLTAGE_VALUE = 5.0;    // 0 V .. +5 V is output range
+    const float64   MIN_VOLTAGE_VALUE = 0.0;
+
+    int32       error=0;
+    char       errBuff[2048]={'\0'};
+
+    TaskHandle  hTaskInput = 0;
+    TaskHandle  hTaskOutput = 0;
+
+    float64     average=0;
+    float64     voltageValue=0;
+
+    int i, l;
+    int32 read;
+    int vStart, vStop, step = 10, nPoints;
+
 };
 
 #endif // BWOEXPERIMENT_H
