0e7d4e053d66213469a85b4bbe01721f54ee0b33
NazariiBoichuk <nazariy.boychuk.a@gmail.com>
2018-07-19 18:35:55 +0200

    stop function, minor changes in toDo()
    Add a few constants and variables that are useful (avoid magic numbers). Add voltage reset in stop method. Add minor comments.

--- BWOSetup/BWOExperiment.cpp
+++ BWOSetup/BWOExperiment.cpp
@@ -4,6 +4,9 @@
 #include <QDebug>
 #include <QThread>
 
+// current realization of the error detection
+// DAQmxFailer returns true when functionCall returns the value < 0
+// Values > 0 are warnings
 #define DAQmxErrChk(functionCall) if( DAQmxFailed(error=(functionCall)) ) throw error
 
 BWOExperiment::BWOExperiment()
@@ -13,8 +16,11 @@ BWOExperiment::BWOExperiment()
 
 void BWOExperiment::toDo()
 {
+    // Concept of working with NI DAQmx: Create Task -> Configurate its channel(s) -> Start Task ->
+    // -> Write/Read/other actions with the channel(s) -> Stop Task -> Clear Task */
+
     // Sending signal that experiment has been started
-    emit ExperimentStarted();
+   /*emit ExperimentStarted();
 
     try
     {
@@ -58,48 +64,82 @@ void BWOExperiment::toDo()
     }
 
     // emitting signal that experiment ended
-    emit ExperimentFinished();
+    emit ExperimentFinished();*/
     qDebug() << "To do function ended";
 }
 
 void BWOExperiment::toDo(QObject *expSettings)
 {
+    // Concept of working with NI DAQmx: Create Task ->
+    // -> Configurate its channel(s) -> Start Task ->
+    // -> Write/Read/other actions with the channel(s) ->
+    // -> Stop Task -> Clear Task
+
+    // Sending signal that experiment has been started
     emit ExperimentStarted();
 
     try
     {
-        DAQmxErrChk (DAQmxCreateTask("",&hTaskInput));
-        DAQmxErrChk (DAQmxCreateAIVoltageChan(hTaskInput,"Dev1/ai3","",DAQmx_Val_Cfg_Default,MIN_VOLTAGE_VALUE,MAX_VOLTAGE_VALUE,DAQmx_Val_Volts,NULL));
+        // Configuration
+        DAQmxErrChk (DAQmxCreateTask("Input Voltage Task", &hTaskInput));
+        DAQmxErrChk (DAQmxCreateAIVoltageChan(hTaskInput, "Dev1/ai3", "", DAQmx_Val_Cfg_Default, MIN_VOLTAGE_VALUE, MAX_VOLTAGE_VALUE, DAQmx_Val_Volts, NULL));
         DAQmxErrChk (DAQmxStartTask(hTaskInput));
 
-        DAQmxErrChk (DAQmxCreateTask("Output Voltage Task",&hTaskOutput));
-        DAQmxErrChk (DAQmxCreateAOVoltageChan(hTaskOutput,"Dev1/ao0","", MIN_VOLTAGE_VALUE,MAX_VOLTAGE_VALUE,DAQmx_Val_Volts,""));
+        DAQmxErrChk (DAQmxCreateTask("Output Voltage Task", &hTaskOutput));
+        DAQmxErrChk (DAQmxCreateAOVoltageChan(hTaskOutput, "Dev1/ao0", "", MIN_VOLTAGE_VALUE, MAX_VOLTAGE_VALUE, DAQmx_Val_Volts, ""));
         DAQmxErrChk (DAQmxStartTask(hTaskOutput));
-        DAQmxErrChk (DAQmxWriteAnalogScalarF64(hTaskOutput,false,4,1.543,NULL));
-        QThread::msleep(5000);
-        for(int i = 1; i <= 30; i++)
+
+
+        QThread::msleep(1000);
+        // TO DO:
+        // Get this information from the Window
+        lowestVoltage = 0;
+        hightesVoltage = 3;
+        numberPoints = 7;
+        float64 delta = (hightesVoltage - lowestVoltage) / (numberPoints - 1);
+
+        for(int32 i = 0; i < numberPoints; i++)
         {
-            DAQmxErrChk (DAQmxWriteAnalogScalarF64(hTaskOutput,false,4,i*0.1,NULL));
+            //TO DO: add averaging
+
+            average = lowestVoltage + delta * i;
+            DAQmxErrChk (DAQmxWriteAnalogScalarF64(hTaskOutput, false, TIMEOUT, average, NULL));
             QThread::msleep(500);
-            qDebug() << "Current voltage value is " << i*0.1;
-            //            cout << i*0.001;
-            //            DAQmxErrChk (DAQmxReadAnalogScalarF64(hTaskInput,4,&voltageValue,NULL));
-            //            fout << i*0.1 << "\t" << voltageValue << endl;
-            //            Sleep(1000);
-            double progress = i / 30.0 * 100.0;
+            qDebug() << "Current voltage value is " << average;
+
+            DAQmxErrChk (DAQmxReadAnalogScalarF64(hTaskInput, TIMEOUT, &voltageValue, NULL));
+
+            qDebug() << "Current read voltage value is " << voltageValue;
+
+            double progress = (i + 1) / numberPoints * 100.0;
             emit ProgressChanged(progress);
         }
     }
     catch(int error)
     {
         DAQmxGetExtendedErrorInfo(errBuff,2048);
-        qDebug() << "DAQmx Error: " << errBuff;
+        qDebug() << "DAQmx Error: " << errBuff << endl;
     }
     catch (...)
     {
         qDebug() << "Unknown exception caught\n";
-    }
+    }    
 
+    // emitting signal that experiment ended
+    emit ExperimentFinished();
+
+    qDebug() << "To do function ended";
+
+    this->stop();
+}
+
+void BWOExperiment::stop()
+{
+    // To DO:
+    // Add voltage reset when experiment is finished: set to zero
+    // WHERE SHOULD THIS FUNCTION BE USED???
+    DAQmxErrChk (DAQmxWriteAnalogScalarF64(hTaskOutput,false,4,0,NULL));
+    QThread::msleep(500);
     if( hTaskInput!=0 )  {
         DAQmxStopTask(hTaskInput);
         DAQmxClearTask(hTaskInput);
@@ -108,17 +148,7 @@ void BWOExperiment::toDo(QObject *expSettings)
         DAQmxStopTask(hTaskOutput);
         DAQmxClearTask(hTaskOutput);
     }
-
-    emit ExperimentFinished();
-
-    qDebug() << "To do function ended";
-}
-
-void BWOExperiment::stop()
-{
-    // To DO:
-    // Add voltage reset when experiment is finished
-
+    qDebug() << "Voltage was reset. Tasks are cleaned.";
     IExperiment::stop();
 }
 
--- BWOSetup/BWOExperiment.h
+++ BWOSetup/BWOExperiment.h
@@ -3,7 +3,7 @@
 
 #include "IExperiment.h"
 
-// Working with NI DAQ MX
+// Working with NI DAQmx
 #include<NIDAQmx.h>
 
 class BWOExperiment : public IExperiment
@@ -16,16 +16,20 @@ public:
 
 private:
     const float64   MAX_VOLTAGE_VALUE = 5.0;    // 0 V .. +5 V is output range
-    const float64   MIN_VOLTAGE_VALUE = 0.0;
+    const float64   MIN_VOLTAGE_VALUE = 0.0;    // Probably we need to use another ranges for the input range (Single-ended ±10 V in specifications to USB-6008)
+    const float64   TIMEOUT = 4.0;              // Waiting time for one read/write action in seconds
 
     int32       error=0;
-    char       errBuff[2048]={'\0'};
+    char        errBuff[2048]={'\0'};
 
     TaskHandle  hTaskInput = 0;
     TaskHandle  hTaskOutput = 0;
 
-    float64     average=0;
-    float64     voltageValue=0;
+    float64     average = 0;
+    float64     voltageValue = 0;
+    float64     lowestVoltage = 0;
+    float64     hightesVoltage = 0;
+    int32       numberPoints = 0;
 
     int i, l;
     int32 read;
