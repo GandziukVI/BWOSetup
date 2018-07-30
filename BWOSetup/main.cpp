b49c8c154bfc079914bccb2caa4e9ec3dd365e65
GandziukVI <gandziuk2009@gmail.com>
2018-07-30 19:03:49 +0200

    Improving functionality
    Connecting BWOExperiment properties and start() and stop() slots to GUI

--- BWOSetup/main.cpp
+++ BWOSetup/main.cpp
@@ -1,10 +1,12 @@
 #include <QtWidgets/QApplication>
+#include <QtQuick/QQuickItem>
 #include <QtQuick/QQuickView>
 #include <QtCore/QDir>
 #include <QtQml/QQmlContext>
 #include <QtQml/QQmlEngine>
 
 #include "BWOExperiment.h"
+#include "qmlBackEnd/BWOExpModel.h"
 
 int main(int argc, char **argv)
 {
@@ -12,22 +14,34 @@ int main(int argc, char **argv)
 
     QApplication app(argc, argv);
 
+    // Attaching QML back-end
+
+    qmlRegisterType<BWOExpModel>("BWOModel", 1, 0, "BWOExpModel");
+
+    BWOExpModel dataModel;
+    BWOExperiment bwoExperiment(&dataModel);
+
     QQuickView viewer;
     QObject::connect(viewer.engine(), &QQmlEngine::quit, &viewer, &QWindow::close);
+
+    viewer.rootContext()->setContextProperty("dataModel", &dataModel);
 
     QUrl source = QUrl("qrc:/qml/main.qml");
 
     viewer.setTitle(QStringLiteral("Backward Wave Oscillator (BWO)"));
-
     viewer.setSource(source);
     viewer.setResizeMode(QQuickView::SizeRootObjectToView);
+
+    QObject *item = viewer.rootObject();
+
+    QObject::connect(item, SIGNAL(startButtonClicked()), &bwoExperiment, SLOT(start()));
+    QObject::connect(item, SIGNAL(stopButtonClicked()), &bwoExperiment, SLOT(stop()));
+
     viewer.show();
 
     // For testing purpose
     // see output in debug console
-    QObject *expSettings = new QObject();
-    BWOExperiment experiment(expSettings);
-    experiment.start();
+
 
     return app.exec();
 }
