33afcf8c1ec32fe52315ab29956210b22fd1992c
NazariiBoichuk <nazariy.boychuk.a@gmail.com>
2018-08-01 12:26:46 +0200

    Add comments to IExp header

--- BWOSetup/iexperiment.h
+++ BWOSetup/iexperiment.h
@@ -1,18 +1,25 @@
 #ifndef IEXPERIMENT_H
 #define IEXPERIMENT_H
 
-#include <QFuture>
-#include <QMutex>
-#include <QObject>
+#include <QFuture>      // allows threads to be synchronized against one or more results which will be ready at a later point in time.
+#include <QObject>      // is the base class of all Qt objects.
+#include <QMutex>       // is to protect an object, data structure or section of code so that only one thread can access it at a time. Delete?
 
+// The Meta-Object Compiler (moc) tool reads a C++ header file. If it finds one or more
+// class declarations that contain the Q_OBJECT macro, it produces a C++ source file
+// containing the meta-object code for those classes. Meta-object code is required for
+// the signals and slots mechanism, the run-time type information, and the dynamic property system.
+
+/* IExperiment class is the basic class for a single experiment realization
+ * It introduces the most important methods, signals, and slots */
 class IExperiment : public QObject
 {
     Q_OBJECT
 public:
-    IExperiment(QObject *expSettings);
+    IExperiment(QObject *expSettings);              // ?? Why do we need a constructor with Settings if we launch toDo with settings?
     ~IExperiment();
 
-public slots:
+public slots:                                       // Slots those should react on signals/other action to run and stop the exp.
     virtual void start();
     virtual void stop();
 
@@ -21,14 +28,14 @@ protected:
     QObject *mExpSettings;
 
     IExperiment();
-    virtual void toDo(QObject *expSettings) = 0;
+    virtual void toDo(QObject *expSettings) = 0;    // The main logic of the experiment should be relized in this method
 
 private:
-    mutable QMutex mIExperimentMutex;
-    QFuture<void>  mExpThreadRes;
+    mutable QMutex mIExperimentMutex;               // !! Never used. What is it for?
+    QFuture<void>  mExpThreadRes;                   // represents the result of an asynchronous computation
 
 signals:
-    void StatusChanged(QString &statusString);
+    void StatusChanged(QString &statusString);      // !! Never used. TO DO: create the field on the form to display the current status
     void ProgressChanged(double progressValue);
     void ExperimentStarted();
     void ExperimentFinished();
